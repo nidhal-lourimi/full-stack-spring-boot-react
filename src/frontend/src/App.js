a1a73a5768ae228c6c6df6714006987cbc29a6d0
nidhal-lourimi <52574003+nidhal-lourimi@users.noreply.github.com>
2021-10-17 17:52:41 +0100

    react button with ant design

---
+++ src/frontend/src/App.js
@@ -0,0 +1,12 @@
+import {Button} from "antd";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Button type='primary'>hello</Button>
+    </div>
+  );
+}
+
+export default App;
