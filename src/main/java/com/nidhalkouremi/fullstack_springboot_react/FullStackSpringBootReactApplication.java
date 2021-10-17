5d99028e51a229fb7d775b9bc5d67cc0e257b0e1
nidhal-lourimi <52574003+nidhal-lourimi@users.noreply.github.com>
2021-10-17 13:57:56 +0100

    first commit

---
+++ src/main/java/com/nidhalkouremi/fullstack_springboot_react/FullStackSpringBootReactApplication.java
@@ -0,0 +1,13 @@
+package com.nidhalkouremi.fullstack_springboot_react;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackSpringBootReactApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullStackSpringBootReactApplication.class, args);
+    }
+
+}
