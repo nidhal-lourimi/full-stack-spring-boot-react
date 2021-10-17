7c722e20baa6bd26763fc7c4a17d16c5557b7529
nidhal-lourimi <52574003+nidhal-lourimi@users.noreply.github.com>
2021-10-17 18:13:14 +0100

    react Button using antd

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,10 +1,16 @@
-import {Button} from "antd";
+import {Button,Radio} from "antd";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Button type='primary'>hello</Button>
+        <br/>
+        <Radio.Group value='large' >
+            <Radio.Button value="large">Large</Radio.Button>
+            <Radio.Button value="default">Default</Radio.Button>
+            <Radio.Button value="small">Small</Radio.Button>
+        </Radio.Group>
     </div>
   );
 }
