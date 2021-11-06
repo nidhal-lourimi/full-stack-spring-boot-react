74ee38f3b83eb9e36e15ad091b94777027b669c9
nidhal-lourimi <52574003+nidhal-lourimi@users.noreply.github.com>
2021-11-06 04:43:59 +0100

    CI testing

--- src/test/java/com/nidhallouremi/fullstack_springboot_react/FullStackSpringBootReactApplicationTests.java
+++ src/test/java/com/nidhallouremi/fullstack_springboot_react/FullStackSpringBootReactApplicationTests.java
@@ -1,5 +1,6 @@
 package com.nidhallouremi.fullstack_springboot_react;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,8 @@ class FullStackSpringBootReactApplicationTests {
 
     @Test
     void contextLoads() {
+
+        Assertions.fail("try again ");
     }
 
 }
